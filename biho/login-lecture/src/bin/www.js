66dd459f3fdffcc9ac39106d7616de149a8832ef
hobiJeong <jjb26433@naver.com>
2023-07-05 22:38:32 +0900

    폴더 구조 최적화

--- biho/login-lecture/src/bin/www.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-
-const app = require("../app"); // app.js의 app을 받아옴
-const PORT = 3000; //포트번호
-
-app.listen(PORT, () => {
-    console.log("서버 가동");
-});
-// listen으로 서버 띄움. 2번째 파라미터에 콜백함수
