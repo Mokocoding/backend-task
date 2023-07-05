a43722789f6315a2eaf9e44fc38edd92f8d8d9af
hobiJeong <jjb26433@naver.com>
2023-07-06 04:33:55 +0900

    회원가입 라우팅 구현

--- biho/login-lecture/app/src/routes/home/index.js
+++ biho/login-lecture/app/src/routes/home/index.js
@@ -8,7 +8,8 @@ const ctrl = require("./home.ctrl");
 router.get("/", ctrl.output.home);
 router.get("/login", ctrl.output.login);
 router.get("/register", ctrl.output.register);
-router.post("/login", ctrl.process.login);
 
+router.post("/login", ctrl.process.login);
+router.post("/register", ctrl.process.register);
 
 module.exports = router; // 외부파일에서 사용 가능하도록 위해 외부로 내보내기.
