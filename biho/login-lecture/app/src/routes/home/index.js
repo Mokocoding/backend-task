3dad828503c840ee904a08e7634516eb4ae1d5a7
hobiJeong <jjb26433@naver.com>
2023-07-06 00:14:45 +0900

    로그인을 요청하는 fetch 생성

--- biho/login-lecture/app/src/routes/home/index.js
+++ biho/login-lecture/app/src/routes/home/index.js
@@ -7,8 +7,8 @@ const ctrl = require("./home.ctrl");
 
 router.get("/", ctrl.home);
 //get으로 루트경로 생성. render 메서드를 통해 home/index파일 띄움
-
 router.get("/login", ctrl.login);
 //get으로 login경로 생성. render 메서드를 통해 home/login파일 띄움
+
 
 module.exports = router; // 외부파일에서 사용 가능하도록 위해 외부로 내보내기.
