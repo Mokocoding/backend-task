c26666fae3a88f5057a9e657caded7bd3f1b2a27
hobiJeong <jjb26433@naver.com>
2023-07-05 22:47:20 +0900

    프런트 단 javascript 파일 만들기

--- biho/login-lecture/app/app.js
+++ biho/login-lecture/app/app.js
@@ -12,6 +12,7 @@ const home = require("./src/routes/home");
 // 앱 세팅
 app.set("views", "./src/views"); //view 파일, 파일이 저장될 폴더 이름
 app.set("view engine", "ejs"); //html 코드를 어떤 엔진으로 해석할지 정함. view engine을 ejs엔진 사용
+app.use(express.static(`${__dirname}/src/public`))
 
 app.use("/", home); // use -> 미들 웨어를 등록해주는 메서드. 루트 경로로 오면 home으로 이동
 
