ea5846d1453b3400888695492e69ba94bb10584b
jaegyun0320 <zkdbqhdb@naver.com>
2023-07-06 23:30:55 +0900

    회원가입 라우팅 구현

--- jaegyun/node/login-lecture/app/src/routes/home/index.js
+++ jaegyun/node/login-lecture/app/src/routes/home/index.js
@@ -8,6 +8,7 @@ const ctrl = require("./home.ctrl");
 router.get("/", ctrl.output.home);
 router.get("/login", ctrl.output.login);
 router.get("/register", ctrl.output.register);
-router.post("/login", ctrl.process.login); //컨트롤러 부분을 모듈화한다.
 
+router.post("/login", ctrl.process.login); //컨트롤러 부분을 모듈화한다.
+router.post("/register", ctrl.process.register);
 module.exports = router; //외부로 내보내주는 명령
