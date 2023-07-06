2d286025f67345f2ce4d2ea97d7be1743b52265d
jaegyun0320 <zkdbqhdb@naver.com>
2023-07-06 23:07:01 +0900

    회원가입 요청 구현

--- jaegyun/node/login-lecture/app/src/public/js/home/register.js
+++ jaegyun/node/login-lecture/app/src/public/js/home/register.js
@@ -2,19 +2,23 @@
 //  DOM -> Document Object Model (문서 객체 모델)
 
 const id =document.querySelector("#id"), //#id = 선택자 // # - <input id = "id">
+    name = document.querySelector("#name"),
     paword = document.querySelector("#psword"),
-    loginBtn = document.querySelector("#button");
+    confirmPaword = document.querySelector("#confirm-psword"),
+    registerBtn = document.querySelector("#button");
+    
+registerBtn.addEventListener("click", register);
 
-loginBtn.addEventListener("click", login);
-
-function login() {
+function register() {
     const req ={
         id: id.value,
+        name: name.value,
         psword: psword.value,
+        confirmPaword: confirmPaword.value,
     };
     
 
-    fetch("/login", {
+    fetch("/register", {
         method: "POST",
         headers: {
             "Content-Type": "application/json",
@@ -24,12 +28,12 @@ function login() {
         .then((res) => res.json())
         .then((res) => {
             if (res.success){
-                location.href = "/";
+                location.href = "/login";
             } else {
               alert(res.msg);
             }
         })
         .catch((err) => {
-            console.error("로그인 중 에러 발생");
+            console.error("회원가입 중 에러 발생");
         });
 }
